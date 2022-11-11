c9fc573eca1d600ca81bc085ae4ea19e750ca96d
Nathalya Amorim <nathalyamorim87@gmail.com>
2022-11-11 13:43:55 -0500

    main  global variables

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "9124b9093amsh36512cd2a39134fp19030fjsnbcf818ca5c12",
-    "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
-  },
-};
+(() => {
 
-fetch("https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')
+
+  
+})()
